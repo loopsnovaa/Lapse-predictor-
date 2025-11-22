16478e1dc17c81f1a5b65ca01da39e4fbd543cb6
loopsnovaa <bvmotif@gmail.com>
2025-11-22 22:46:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -284,10 +284,10 @@ def performance_page():
             """
             
         c1.markdown(metric_box("Accuracy", f"{row['Accuracy']:.1%}"), unsafe_allow_html=True)
-        c2.markdown(metric_box("Precision", f"{row['Precision']:.1f%}"), unsafe_allow_html=True)
-        c3.markdown(metric_box("Recall", f"{row['Recall']:.1f%}"), unsafe_allow_html=True)
-        c4.markdown(metric_box("F1 Score", f"{row['F1 Score']:.1f%}"), unsafe_allow_html=True)
-        c5.markdown(metric_box("AUC", f"{row['AUC']:.1f%}"), unsafe_allow_html=True)
+        c2.markdown(metric_box("Precision", f"{row['Precision']:.3f}"), unsafe_allow_html=True)
+        c3.markdown(metric_box("Recall", f"{row['Recall']:.3f}"), unsafe_allow_html=True)
+        c4.markdown(metric_box("F1 Score", f"{row['F1 Score']:.3f}"), unsafe_allow_html=True)
+        c5.markdown(metric_box("AUC", f"{row['AUC']:.3f}"), unsafe_allow_html=True)
         
         st.markdown("<br>", unsafe_allow_html=True)
 
